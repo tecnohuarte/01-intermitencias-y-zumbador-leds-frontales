4c5b07b7ee85a55c0e7ff0ff9d096f7e0f727e9e
tecnohuarte <106558201+tecnohuarte@users.noreply.github.com>
2022-06-02 19:10:02 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+})
